e939c589008ef0cf0ea8fcbce2446ed14b1f5913
rahullakhaney <rahul@codebrew.io>
2016-10-21 17:22:54 +0400

    Added Is It Prime App

---
+++ OptionalsPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number: Int?
+
+print (number)
+
+let userEnteredText = "3"
+let userEnteredInteger = Int(userEnteredText)
+
+if let catAge = userEnteredInteger {
+    
+    print (catAge * 7)
+    
+} else {
+    
+    // Show an error message to the users
+    
+}
