c780332673dd8ad479b7e8a18b87b447a21f144b
rahullakhaney <rahul@codebrew.io>
2016-10-21 17:47:55 +0400

    Added Menu Bar Project

---
+++ Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Rahul Lakhaney on 21/10/16.
+//  Copyright © 2016 Rahul Lakhaney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func cameraButton(_ sender: AnyObject) {
+        
+        print("Camera Pressed")
+    
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var timer = Timer()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- samplePlayground.playground/Contents.swift
+++ samplePlayground.playground/Contents.swift
@@ -27,3 +27,5 @@ while i < number {
 }
 
 print (isPrime)
+
+
