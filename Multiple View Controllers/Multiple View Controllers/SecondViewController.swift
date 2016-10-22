933582c7d9508a30649dcee7e1651ab23d088185
rahullakhaney <rahul@codebrew.io>
2016-10-23 01:29:55 +0400

    Added Keyboard Controller and Multiple View Controller

---
+++ Multiple View Controllers/Multiple View Controllers/SecondViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.swift
+//  Multiple View Controllers
+//
+//  Created by Rahul Lakhaney on 23/10/16.
+//  Copyright © 2016 Rahul Lakhaney. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        print("Second View Controller Loaded")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Multiple View Controllers/Multiple View Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiple View Controllers
+//
+//  Created by Rahul Lakhaney on 23/10/16.
+//  Copyright © 2016 Rahul Lakhaney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
